6b59044d9ea00c27e76cb445aee0cfc3cb2d13fa
avinashjindal786 <avinashjindal786@gmail.com>
2022-10-01 17:15:28 +0530

    Initial Commit

---
+++ war-challange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  war-challange
+//
+//  Created by Avinash jindal on 01/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war-challange/war_challangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challangeApp.swift
+//  war-challange
+//
+//  Created by Avinash jindal on 01/10/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
