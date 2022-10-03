be94baec77226b93ca337ad57766169cdd44cb89
avinashjindal786 <avinashjindal786@gmail.com>
2022-10-03 17:28:13 +0530

    first commit

--- war-challange/ContentView.swift
+++ war-challange/ContentView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(){
+            Image("background").ignoresSafeArea()
+            VStack(){
+                Spacer()
+                Image("logo")
+                 Spacer()
+                HStack {
+                    Spacer()
+                    Image("card14")
+                        
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack(){
+                    Spacer()
+                    VStack(spacing: 9.0){
+                        Text("Player")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                    
+                        Text("0").font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            
+                    }
+                    Spacer()
+                    VStack(spacing: 9.0){
+                        Text("Player")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                    
+                        Text("0").font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            
+                    }
+                    Spacer()
+                }
+                Spacer()
+                
+            }
+            
+            
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
     }
 }
